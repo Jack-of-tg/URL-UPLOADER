e9b283b49ee2c3e3dd22b029e10a1f842f3add12
ᎫᎪᏟᏦ <69492163+unknownkidnapper@users.noreply.github.com>
2021-01-31 09:32:12 +0530

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,48 @@
+class Translation(object):
+
+    START_TEXT = """Hello,
+
+This is a Telegram URL Upload Bot!
+
+<b>Please send me any Direct download URL link, I can upload to telegram as File/Video</b>
+
+/help for more details..
+
+Support Group : @TroJanzSupport
+"""
+
+    HELP_USER = """Hai I'am a URL Uploader bot..
+    
+1. Send url (Link | New Name with Extension).
+2. Send Custom Thumbnail (Optional).
+3. Select the button.
+   SVideo - Give File as video with Screenshots
+   DFile  - Give File with Screenshots
+   Video  - Give File as video without Screenshots
+   DFile  - Give File without Screenshots
+
+Support Group : @TroJanzSupport
+"""
+
+    FORMAT_SELECTION = """Select the desired format: <a href='{}'>file size might be approximate</a>
+    
+Send your custum thumbnail if required.
+You can use /deletethumbnail to delete the auto-generated thumbnail."""
+    
+    SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
+URL | newfilename | username | password"""
+
+
+    DOWNLOAD_START = "Downloading..."
+    
+    UPLOAD_START = "Uploading now.."
+    
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \n\nUploaded in {} seconds."
+
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
+
+    CUSTOM_CAPTION_UL_FILE = " "
+
+    SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
+
+    NO_VOID_FORMAT_FOUND = "ERROR...\n<b>YouTubeDL</b> said: {}"
